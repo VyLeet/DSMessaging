4fa42de52caacbf62da6cdcb1aa1a6de986b2b2f
VyLeet <n.vysokinskyi@gmail.com>
2022-12-01 22:21:28 +0200

    Refactor message. Split part of code into extension

--- Sources/App/Model/Message.swift
+++ Sources/App/Model/Message.swift
@@ -10,35 +10,41 @@ import Vapor
 struct Message: Content, Identifiable {
     let text: String
     var id: Int
-    static var listLock = NSLock()
-    static var counterLock = NSLock()
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.text = try container.decode(String.self, forKey: .text)
-        self.id = (try? container.decode(Int.self, forKey: .id)) ?? (Message.getId() ?? 0).advanced(by: 1)
+        self.id = (try? container.decode(Int.self, forKey: .id)) ?? Self.generateID()
     }
+}
+
+extension Message {
+    static var listLock = NSLock()
+    static var counterLock = NSLock()
     
     static private(set) var list = [Message]()
-    static private(set) var counter = 0
+    static private var counter = 0
     
-    static func getId() -> Int {
-        counterLock.lock()
-        counter += 1
-        counterLock.unlock()
+    static private func generateID() -> Int {
+        self.counterLock.lock()
+        defer { self.counterLock.unlock() }
+        
+        self.counter += 1
         return counter
     }
 
     static func log(_ message: Message) {
-        listLock.lock()
-        if (list.contains(where: { $0.id == message.id }) == false)
-        {
-            list.append(message)
-            if (list.count >= 2 && (list[list.count-1].id < list[list.count-2].id)) 
-            {
-                list.sort(by: { $0.id < $1.id })
-            }
+        self.listLock.lock()
+        defer { self.listLock.unlock() }
+        
+        guard false == self.list.contains(where: { $0.id == message.id }) else {
+            return
         }
-        listLock.unlock()
+        
+        self.list.append(message)
+        
+        if self.list.count >= 2 && self.list[list.count - 1].id < self.list[list.count - 2].id {
+            self.list.sort { $0.id < $1.id }
+        }
     }
 }
